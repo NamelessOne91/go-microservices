2d79d168fa691dad72a35dd3f739745727303b12
paolo invernizzi <paolo.invernizzi@emsibg.com>
2023-05-24 17:59:56 +0200

    Add Repository pattern implementaion for testing

--- authentication-service/cmd/api/setup_test.go
+++ authentication-service/cmd/api/setup_test.go
@@ -3,9 +3,15 @@ package main
 import (
 	"os"
 	"testing"
+
+	"github.com/NamelessOne91/auth/data"
 )
 
+var testApp Config
+
 func TestMain(m *testing.M) {
+	repo := data.NewPostgresTestRepository(nil)
+	testApp.Repo = repo
 
 	os.Exit(m.Run())
 }
