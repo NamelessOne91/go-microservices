e6e0dc7631c7a26df0e379ccdeac3a3a6675b1a6
paolo invernizzi <paolo.invernizzi@emsibg.com>
2023-05-26 16:37:29 +0200

    Refactor routing unit tests: use table tests

--- authentication-service/cmd/api/routes_test.go
+++ authentication-service/cmd/api/routes_test.go
@@ -7,31 +7,38 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func routeExists(t *testing.T, routes chi.Router, route string) {
+func routeExists(routes chi.Router, route string) bool {
 	found := false
-
 	_ = chi.Walk(routes, func(method, foundRoute string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
 		if route == foundRoute {
 			found = true
 		}
 		return nil
 	})
-
-	if !found {
-		t.Errorf("did not find %s in registed routes", route)
-	}
+	return found
 }
 
 func Test_routes_exist(t *testing.T) {
+	tests := []struct {
+		name     string
+		route    string
+		expected bool
+	}{
+		{"Route should exist", "/authenticate", true},
+		{"Route does not exist", "/test-route", false},
+	}
 
 	testApp := Config{}
-
 	testRoutes := testApp.routes()
 	chiRoutes := testRoutes.(chi.Router)
 
-	routes := []string{"/authenticate"}
-	for _, route := range routes {
-		routeExists(t, chiRoutes, route)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := routeExists(chiRoutes, tt.route)
+			if got != tt.expected {
+				t.Errorf("expected routeExists('%s') to be %t - got %t", tt.route, tt.expected, got)
+			}
+		})
 	}
 
 }
